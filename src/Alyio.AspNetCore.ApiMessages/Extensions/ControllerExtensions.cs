38ffcaa95d8468cade755394ae3ab1ce4e94faa0
qqbuby <qqbuby@gmail.com>
2022-06-03 20:05:22 +0800

    Migrate Controller to ControllerBase

--- src/Alyio.AspNetCore.ApiMessages/Extensions/ControllerExtensions.cs
+++ src/Alyio.AspNetCore.ApiMessages/Extensions/ControllerExtensions.cs
@@ -6,19 +6,19 @@ using Microsoft.Net.Http.Headers;
 namespace Alyio.AspNetCore.ApiMessages
 {
     /// <summary>
-    /// Extension methods for the <see cref="Controller"/>.
+    /// Extension methods for the <see cref="ControllerBase"/>.
     /// </summary>
     public static class ControllerExtensions
     {
         /// <summary>
         /// Create a created message that produces a Created (201) response.
         /// </summary>
-        /// <param name="controller">the <see cref="Controller"/>.</param>
+        /// <param name="controller">the <see cref="ControllerBase"/>.</param>
         /// <param name="actionName">The name of the action to use for generating the URL.</param>
         /// <param name="routeValues">The content value to format in the entity body.</param>
         /// <param name="id">A <see cref="string"/> value to identity the entity.</param>
         /// <returns>The <see cref="Alyio.AspNetCore.ApiMessages.CreatedMessage"/>.</returns>
-        public static CreatedMessage CreatedMessageAtAction(this Controller controller, string actionName, object routeValues, string id)
+        public static CreatedMessage CreatedMessageAtAction(this ControllerBase controller, string actionName, object routeValues, string id)
         {
             string? url = controller.LinkAtAction(actionName, routeValues);
             return controller.CreatedMessage(url, id);
@@ -27,12 +27,12 @@ namespace Alyio.AspNetCore.ApiMessages
         /// <summary>
         /// Create a created message that produces a Created (201) response.
         /// </summary>
-        /// <param name="controller">the <see cref="Controller"/>.</param>
+        /// <param name="controller">the <see cref="ControllerBase"/>.</param>
         /// <param name="routeName">The name of the route to use for generating the URL.</param>
         /// <param name="routeValues">The content value to format in the entity body.</param>
         /// <param name="id">A <see cref="string"/> value to identity the entity.</param>
         /// <returns>The <see cref="Alyio.AspNetCore.ApiMessages.CreatedMessage"/>.</returns>
-        public static CreatedMessage CreatedMessageAtRoute(this Controller controller, string routeName, object routeValues, string id)
+        public static CreatedMessage CreatedMessageAtRoute(this ControllerBase controller, string routeName, object routeValues, string id)
         {
             string? url = LinkAtRoute(controller, routeName, routeValues);
             return controller.CreatedMessage(url, id);
@@ -41,11 +41,11 @@ namespace Alyio.AspNetCore.ApiMessages
         /// <summary>
         /// Create a created message that produces a Created (201) response.
         /// </summary>
-        /// <param name="controller">the <see cref="Controller"/>.</param>
+        /// <param name="controller">the <see cref="ControllerBase"/>.</param>
         /// <param name="uri">The URL used to get the entity.</param>
         /// <param name="id">A <see cref="string"/> value to identity the entity.</param>
         /// <returns>The <see cref="Alyio.AspNetCore.ApiMessages.CreatedMessage"/>.</returns>
-        public static CreatedMessage CreatedMessage(this Controller controller, string? uri, string id)
+        public static CreatedMessage CreatedMessage(this ControllerBase controller, string? uri, string id)
         {
             controller.Response.StatusCode = 201;
             controller.Response.Headers[HeaderNames.Location] = uri;
@@ -64,12 +64,12 @@ namespace Alyio.AspNetCore.ApiMessages
         /// <summary>
         /// Generates an absolute URL using the specified route name and values.
         /// </summary>
-        /// <param name="controller">The <see cref="Controller"/>.</param>
+        /// <param name="controller">The <see cref="ControllerBase"/>.</param>
         /// <param name="routeName">The name of the route that is used to generate the URL.</param>
         /// <param name="routeValues">An object that contains the route values.</param>
         /// <returns>The generated absolute URL.</returns>
         /// <remarks>The protocol and host is obtained from the current request.</remarks>
-        public static string? LinkAtRoute(this Controller controller, string routeName, object routeValues)
+        public static string? LinkAtRoute(this ControllerBase controller, string routeName, object routeValues)
         {
             return controller.Url.Link(routeName, routeValues);
         }
@@ -77,12 +77,12 @@ namespace Alyio.AspNetCore.ApiMessages
         /// <summary>
         /// Generates an absolute URL using the specified route name and values.
         /// </summary>
-        /// <param name="controller">The <see cref="Controller"/>.</param>
+        /// <param name="controller">The <see cref="ControllerBase"/>.</param>
         /// <param name="actionName">The name of the action method that used to generate URLs.</param>
         /// <param name="routeValues">An object that contains the route values.</param>
         /// <returns>The generated absolute URL.</returns>
         /// <remarks>The protocol and host is obtained from the current request.</remarks>
-        public static string? LinkAtAction(this Controller controller, string actionName, object routeValues)
+        public static string? LinkAtAction(this ControllerBase controller, string actionName, object routeValues)
         {
             return controller.Url.Action(
                                new UrlActionContext
