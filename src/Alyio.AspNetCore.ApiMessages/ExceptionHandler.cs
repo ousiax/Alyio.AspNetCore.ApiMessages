e3a79ec519d6bef9f78fa6544c9fabd13c59d6da
qqbuby <qqbuby@gmail.com>
2022-06-04 17:33:14 +0800

    Refine package references

--- src/Alyio.AspNetCore.ApiMessages/ExceptionHandler.cs
+++ src/Alyio.AspNetCore.ApiMessages/ExceptionHandler.cs
@@ -3,49 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Alyio.AspNetCore.ApiMessages
+namespace Alyio.AspNetCore.ApiMessages;
+
+#pragma warning disable CS1574
+
+/// <summary>
+/// <see cref="Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware"/>.
+/// </summary>
+public static class ExceptionHandler
 {
     /// <summary>
-    /// <see cref="Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware"/>.
+    /// Handle a HTTP context exception and write a <see cref="InternalServerErrorMessage"/> into the <see cref="HttpContext.Response"/>.
     /// </summary>
-    public static class ExceptionHandler
+    /// <param name="context">The <see cref="HttpContext"/></param>
+    /// <returns></returns>
+    /// <remarks><seealso cref="Microsoft.AspNetCore.Builder.ExceptionHandlerOptions.ExceptionHandler"/>.</remarks>
+    public static Task WriteUnhandledMessageAsync(HttpContext context)
     {
-        /// <summary>
-        /// Handle a HTTP context exception and write a <see cref="InternalServerErrorMessage"/> into the <see cref="HttpContext.Response"/>.
-        /// </summary>
-        /// <param name="context">The <see cref="HttpContext"/></param>
-        /// <returns></returns>
-        /// <remarks><seealso cref="Microsoft.AspNetCore.Builder.ExceptionHandlerOptions.ExceptionHandler"/>.</remarks>
-        public static Task WriteUnhandledMessageAsync(HttpContext context)
+        var error = context.Features.Get<IExceptionHandlerFeature>()!.Error;
+
+        var message = new InternalServerErrorMessage(error.Message);
+        message.ApiMessage.ExceptionType = error.GetType().Name;
+        message.ApiMessage.TraceIdentifier = context.TraceIdentifier;
+
+        var errors = new List<string>();
+        if (error is AggregateException aggregateException)
         {
-            var error = context.Features.Get<IExceptionHandlerFeature>()!.Error;
-
-            var message = new InternalServerErrorMessage(error.Message);
-            message.ApiMessage.ExceptionType = error.GetType().Name;
-            message.ApiMessage.TraceIdentifier = context.TraceIdentifier;
-
-            var errors = new List<string>();
-            if (error is AggregateException aggregateException)
+            aggregateException.Flatten().Handle(e =>
             {
-                aggregateException.Flatten().Handle(e =>
-                {
-                    errors.Add(e.Message);
-                    return true;
-                });
-                message.ApiMessage.Errors = errors.Distinct().ToList();
-            }
-
-            if (context.RequestServices.GetService<IWebHostEnvironment>().IsDevelopment())
-            {
-                message.ApiMessage.Detail = error.ToString();
-            }
-
-            return context.WriteApiMessageAsync(message);
+                errors.Add(e.Message);
+                return true;
+            });
+            message.ApiMessage.Errors = errors.Distinct().ToList();
         }
+
+        if (context.RequestServices.GetService<IHostingEnvironment>().IsDevelopment())
+        {
+            message.ApiMessage.Detail = error.ToString();
+        }
+
+        return context.WriteApiMessageAsync(message);
     }
 }
